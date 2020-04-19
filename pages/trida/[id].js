2a4d1bbcb4cf931101687802b74607d4c05cc2a6
awfulthings <cernockyd@gmail.com>
2020-04-19 23:34:27 +0200

    group overview design

--- pages/trida/[id].js
+++ pages/trida/[id].js
@@ -1,155 +1,212 @@
 /** @jsx jsx */
-import StarterLayout from '../../components/StarterLayout'
+import DashboardLayout from '../../components/dashboard/DashboardLayout'
+import GroupHeader from '../../components/dashboard/GroupHeader'
 import { withApollo } from '../../apollo/client'
 import { Flex, Box } from 'reflexbox'
-import Link from 'next/link'
-import { jsx, Text, Button, Heading, Grid } from 'theme-ui'
-import { MenuItem, Icon } from "@blueprintjs/core"
-import { Select } from "@blueprintjs/select"
+// import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { jsx, Text, Heading, Grid } from 'theme-ui'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
+import FullPageLoading from "../../components/FullPageLoading";
 
-const students = [
-  {id: '', name: 'Dalibor Černocký'},
-  {id: '', name: 'Adam Melničák'},
-  {id: '', name: 'Chief Keef'},
-  {id: '', name: 'Adam Melničák'},
-  {id: '', name: 'John Lennon'},
-  {id: '', name: 'Dalibor Černocký'},
-  {id: '', name: 'Adam Melničák'},
-  {id: '', name: 'Chief Keef'},
-  {id: '', name: 'Adam Melničák'},
-  {id: '', name: 'John Lennon'},
-  {id: '', name: 'Dalibor Černocký'},
-  {id: '', name: 'Adam Melničák'},
-  {id: '', name: 'Někdo'},
-  {id: '', name: 'Chief Keef'},
-  {id: '', name: 'Adam Melničák'},
-  {id: '', name: 'John Lennon'},
-  {id: '', name: 'Dalibor Černocký'},
-  {id: '', name: 'Adam Melničák'},
-  {id: '', name: 'Chief Keef'},
-  {id: '', name: 'Adam Melničák'},
-  {id: '', name: 'John Lennon'},
+export const GROUP = gql`
+  query Group($id: ID!) {
+    group(id: $id) {
+      id
+      name
+    }
+  }
+`
+
+const SuccessIcon = () => (
+  <svg
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      stroke="green"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      shapeRendering="geometricPrecision"
+      style={{color:'green', fill: 'green', stroke: '#fff'}}>
+      <path
+        d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z"
+        fill="green"
+        stroke="green">
+      </path>
+      <path
+        d="M8 11.8571L10.5 14.3572L15.8572 9"
+        fill="none" stroke="#fff">
+      </path>
+    </svg>
+)
+
+const topics = [
+  {
+    name: 'Fáze práce s informacemi',
+    subtopics: [
+      'Vyhledávání na internetu I ',
+      'Vyhledávání na internetu II: Kde vyhledávat?',
+      'Vyhledávání na internetu III: Jak vyhledávat?',
+      'Filtrování výsledků',
+      'Hodnocení informací',
+      'Hodnocení informací: Dezinformace a manipulace s informacemi',
+      'Hodnocení informací: Wikipedie',
+      'Využití informací',
+      'S informacemi k řešení problému',
+    ]
+  },
+  {
+    name: 'Fáze práce s informacemi',
+    subtopics: [
+      'Vyhledávání na internetu I ',
+      'Vyhledávání na internetu II: Kde vyhledávat?',
+      'Vyhledávání na internetu III: Jak vyhledávat?',
+      'Filtrování výsledků',
+      'Hodnocení informací',
+      'Hodnocení informací: Dezinformace a manipulace s informacemi',
+      'Hodnocení informací: Wikipedie',
+      'Využití informací',
+      'S informacemi k řešení problému',
+    ]
+  },
+  {
+    name: 'Práce s informacemi a učení',
+    subtopics: [
+      'Vyhledávání na internetu I ',
+      'Vyhledávání na internetu II: Kde vyhledávat?',
+      'Vyhledávání na internetu III: Jak vyhledávat?',
+      'Filtrování výsledků',
+      'Hodnocení informací',
+      'Hodnocení informací: Dezinformace a manipulace s informacemi',
+      'Hodnocení informací: Wikipedie',
+      'Využití informací',
+      'S informacemi k řešení problému',
+    ]
+  },
+  {
+    name: 'Média a občanství',
+    subtopics: [
+      'Vyhledávání na internetu I ',
+      'Vyhledávání na internetu II: Kde vyhledávat?',
+      'Vyhledávání na internetu III: Jak vyhledávat?',
+      'Filtrování výsledků',
+      'Hodnocení informací',
+      'Hodnocení informací: Dezinformace a manipulace s informacemi',
+      'Hodnocení informací: Wikipedie',
+      'Využití informací',
+      'S informacemi k řešení problému',
+    ]
+  },
+  {
+    name: 'Práce s dokumenty',
+    subtopics: [
+      'Vyhledávání na internetu I ',
+      'Vyhledávání na internetu II: Kde vyhledávat?',
+      'Vyhledávání na internetu III: Jak vyhledávat?',
+      'Filtrování výsledků',
+      'Hodnocení informací',
+      'Hodnocení informací: Dezinformace a manipulace s informacemi',
+      'Hodnocení informací: Wikipedie',
+      'Využití informací',
+      'S informacemi k řešení problému',
+    ]
+  },
+  {
+    name: 'Interakce, vzájemnost a zpětná vazba',
+    subtopics: [
+      'Vyhledávání na internetu I ',
+      'Vyhledávání na internetu II: Kde vyhledávat?',
+      'Vyhledávání na internetu III: Jak vyhledávat?',
+      'Filtrování výsledků',
+      'Hodnocení informací',
+      'Hodnocení informací: Dezinformace a manipulace s informacemi',
+      'Hodnocení informací: Wikipedie',
+      'Využití informací',
+      'S informacemi k řešení problému',
+    ]
+  },
 ]
 
-function itemRenderer(group, {modifiers}) {
-  return (
-    <Link href={group.link}><MenuItem
-        active={modifiers.active}
-        key={group.id}
-        text={group.name}
-    /></Link>
-);
-}
+const students = [
+  {id: '', active: false, picture: 'https://lh3.googleusercontent.com/a-/AOh14GgS47uhXbbJUY6kq74FVM2RQhnz3LD01ZA00Pq2tPc', name: 'Dalibor Černocký'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skalf', name: 'John Doe'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skdfsfsa', name: 'Alois Jirásek'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skaldsfadsaf', name: 'Klotylda Ruprechtová'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=sdsfa', name: 'John Lennon'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=sksdfdasf', name: 'Dalibor Černocký'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skadsaflf', name: 'Adam Melničák'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skadasflf', name: 'Chief Keef'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skadsaff', name: 'Adam Melničák'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skalf', name: 'John Lennon'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skaslf', name: 'Dalibor Černocký'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skalssf', name: 'Adam Melničák'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skdalf', name: 'Někdo'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=sksddalf', name: 'Chief Keef'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skbvalf', name: 'Adam Melničák'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skanlf', name: 'John Lennon'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skavbnlf', name: 'Dalibor Černocký'},
+  {id: '', active: true, picture: 'https://i.pravatar.cc/32?u=skalghjf', name: 'Adam Melničák'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skalf', name: 'Chief Keef'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skaldfhf', name: 'Adam Melničák'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skalhdgff', name: 'John Lennon'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skaljdff', name: 'John Lennon'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skajdghlf', name: 'Dalibor Černocký'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skadflf', name: 'Adam Melničák'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skaldfgsf', name: 'Chief Keef'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skalcycvf', name: 'Adam Melničák'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skalsgdff', name: 'John Lennon'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skalfdsgf', name: 'John Lennon'},
+  {id: '', active: false, picture: 'https://i.pravatar.cc/32?u=skaldff', name: 'Dalibor Černocký'},
+]
 
 const Trida = () => {
+  const router = useRouter();
+  const { data, loading, error } = useQuery(GROUP, {variables: {id: router.query.id}})
+
+  if (loading || error) {
+    return <FullPageLoading />
+  }
   return (
-    <StarterLayout>
-      <Flex flexWrap="wrap" sx={{variant: 'styles.decoratedBox', overflow: 'display', background: 'transparent'}}>
-        <div sx={{variant: 'styles.decoratedOverlay', maxHeight: '300px'}}></div>
+    <DashboardLayout
+      header={<GroupHeader />}
+      stickHeaderByDefault>
+      <Flex flexWrap="wrap">
         <Box
-          sx={{variant: 'styles.decoratedcontent'}} 
           maxWidth={1240}
           px={35}
           pt={50}
           alignSelf="center"
           width={[1, 5/6]}
           mx="auto">
-            <Box sx={{mb: '50px'}}>
-              <Select
-                onItemSelect={(item) => {
-                  console.log('selected', item)
-                }}
-                items={[
-                  {link: '/u', name: 'Přehled tříd'},
-                  {link: '/trida/ldksahfhjskldafsldfks', name: 'Jiná třída'},
-                  {link: '/trida/ldksahfhjskldafsldfks', name: 'Jiná třída'},
-                  {link: '/trida/ldksahfhjskldafsldfks', name: 'Jiná třída'},
-                  {link: '/trida/ldksahfhjskldafsldfks', name: 'Jiná třída'}
-                ]}
-                filterable={false}
-                activeItem={{id: 'idecko', name: 'Jméno třídy'}}
-                itemRenderer={itemRenderer}
-                >
-                <Button variant="groupSelect">Změnit třídu <Icon icon="caret-down" iconSize={14} sx={{mb: '3px'}} /></Button>
-              </Select>
-              <Heading as="h3" sx={{fontSize: 5, color: 'background', mb: 4}}>Jméno třídy</Heading>
-              <Text sx={{color: 'background', fontSize: 2, color: 'rgba(255,255,255,0.9)'}}>Kód pro pozvání: 88-KKK</Text>
+            <Box sx={{ mb: '42px' }}>
+              <Heading as="h3" sx={{fontSize: 7, color: 'text', mb: 3}}>Výsledky studenta: {students.find(x => x.active === true).name} ╳</Heading>
+              <Text sx={{fontSize: 4, mb: '42px', color: 'text'}}>Vyberte studenta pro filtrování výsledků</Text>
+              <Grid gab={4} columns={4} sx={{position: 'relative', zIndex: '2'}}>
+                {
+                  students.map((student) => <Box sx={{mb: '12px', position: 'relative', zIndex: 1}}>
+                    {student.picture && <img src={student.picture} sx={{boxShadow: '0 4px 14px 0 rgba(0,0,0,0.1)', opacity: (student.active ? '1' : '.5'), display: 'inline-block', height: '32px', borderRadius: '16px', mr: '14px', mb: -2,}} />}
+                <Text sx={{fontSize: 3, fontWeight: (student.active ? 700 : 400), color: (student.active ? 'text' : 'gray'), textDecoration: (student.active ? 'underline' : 'none'),  display: 'inline-block' }}>{student.name} {student.active && '╳'}</Text>
+                  </Box>)
+                }
+              </Grid>
             </Box>
             <Box sx={{ mb: '50px' }}>
-              <Heading as="h3" sx={{fontSize: 3, color: 'rgba(255,255,255,0.9)'}}>Studenti ({students.length})</Heading>
-              <Box
-                sx={{ variant: 'styles.groupCard' }}
-                >
-                  <Grid gab={4} columns={5}>
-                    {
-                      students.map((student) => <Box>
-                        {student.name}
-                      </Box>)
-                    }
-                  </Grid>
-              </Box>
-            </Box>
-            <Box sx={{ mb: '50px' }}>
-              <Heading as="h3" sx={{fontSize: 3, color: '#333'}}>Výsledky</Heading>
-              <Box
-                sx={{ variant: 'styles.groupCard' }}
-                >
+              <Box>
                   <Grid gap={4} columns={2}>
-                    <Box sx={{mb: 2}}>
-                      <Heading sx={{fontSize: 2, pb: 2, mb: 2, borderBottom: '1px solid #ddd'}}>
-                        Téma 1</Heading>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                    </Box>
-                    <Box sx={{mb: 2}}>
-                      <Heading sx={{fontSize: 2, pb: 2, mb: 2, borderBottom: '1px solid #ddd'}}>
-                        Téma 2</Heading>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                    </Box>
-                    <Box sx={{mb: 2}}>
-                      <Heading sx={{fontSize: 2, pb: 2, mb: 2, borderBottom: '1px solid #ddd'}}>
-                        Téma 3</Heading>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                    </Box>
-                    <Box sx={{mb: 2}}>
-                      <Heading sx={{fontSize: 2, pb: 2, mb: 2, borderBottom: '1px solid #ddd'}}>
-                        Téma 4</Heading>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                    </Box>
-                    <Box sx={{mb: 2}}>
-                      <Heading sx={{fontSize: 2, pb: 2, mb: 2, borderBottom: '1px solid #ddd'}}>
-                        Téma 5</Heading>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                    </Box>
-                    <Box sx={{mb: 2}}>
-                      <Heading sx={{fontSize: 2, pb: 2, mb: 2, borderBottom: '1px solid #ddd'}}>
-                        Téma 6</Heading>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                      <Text>Kvíz A</Text>
-                    </Box>
+                    {topics.map((topic) => 
+                      <Box sx={{mb: 2, variant: 'styles.groupCard' }}>
+                        <Heading sx={{fontSize: 5, pb: 2, mb: 2}}>
+                          {topic.name}
+                        </Heading>
+                          {topic.subtopics.map((subtopic, index) => (
+                            <Flex justifyContent="space-between" sx={{pb: '2px', pt: '6px', px: 2, background: (index % 2 == 1 ? '#fff' : '#f6f6f6'), borderRadius: '6px', }}>
+                              <Box><Text sx={{fontSize: 2}}>{subtopic}</Text></Box>
+                              <Box><SuccessIcon /></Box>
+                            </Flex>
+                          ))}
+                      </Box>)}
                   </Grid>
               </Box>
             </Box>
@@ -162,7 +219,7 @@ const Trida = () => {
             </Box>
         </Box>
       </Flex>
-    </StarterLayout>
+    </DashboardLayout>
   );
   
 }
