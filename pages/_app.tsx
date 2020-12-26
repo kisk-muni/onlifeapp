89943710b25bc50f13f1ce14992efb69167b94d6
cernockyd <24832873+cernockyd@users.noreply.github.com>
2020-12-26 14:43:19 +0100

    add core ga tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,121 +1,131 @@
-import React from 'react'
+import React, { useEffect }  from 'react'
 import App from 'next/app'
 import { ThemeProvider } from 'theme-ui'
 import theme from 'theme'
 import { DefaultSeo } from 'next-seo'
 import SEO from 'next-seo.config'
 import "node_modules/@blueprintjs/core/lib/css/blueprint.css"
+import { useRouter } from 'next/router'
+import * as gtag from 'utils/gtag'
 
-export default class MyApp extends App {
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  return (
+    <ThemeProvider theme={theme}>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        .toaster-notification {
+          margin-top: -10px;
+        } 
+        .bp3-toast-container.bp3-toast-container-top.toaster-notification .bp3-toast {
+          box-shadow: 0 8px 30px rgba(0,0,0,0.12);
+        }
+        .bp3-popover {
+          box-shadow: 0 8px 30px rgba(0,0,0,0.12);
+        }
+        .bp3-popover .bp3-popover-arrow-border {
+          fill: #ffffff;
+        }
+        .bp3-popover.popover-success .bp3-popover-arrow-fill {
+          fill: rgb(220, 240, 232);
+        }
+        .bp3-menu-item:hover, .bp3-submenu .bp3-popover-target.bp3-popover-open > .bp3-menu-item {
+          background-color: rgba(233, 233, 233, 0.6);
+        }
+        .bp3-menu-item {
+          padding: 8px 12px;
+        }
+        .bp3-menu {
+          padding: 5px 0;
+        }
+        .bp3-menu-header {
+          padding: 8px 12px 8px 12px;
+        }
+        .bp3-overlay-backdrop {
+          background-color: rgba(0, 0, 0, 0.2);
+        }
+        /*
+          * react-circular-progressbar styles
+        * All of the styles in this file are configurable!
+        */
 
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <ThemeProvider theme={theme}>
-          <DefaultSeo {...SEO} />
-          <Component {...pageProps} />
-          <style jsx global>{`
-            .toaster-notification {
-              margin-top: -10px;
-            } 
-            .bp3-toast-container.bp3-toast-container-top.toaster-notification .bp3-toast {
-              box-shadow: 0 8px 30px rgba(0,0,0,0.12);
-            }
-            .bp3-popover {
-              box-shadow: 0 8px 30px rgba(0,0,0,0.12);
-            }
-            .bp3-popover .bp3-popover-arrow-border {
-              fill: #ffffff;
-            }
-            .bp3-popover.popover-success .bp3-popover-arrow-fill {
-              fill: rgb(220, 240, 232);
-            }
-            .bp3-menu-item:hover, .bp3-submenu .bp3-popover-target.bp3-popover-open > .bp3-menu-item {
-              background-color: rgba(233, 233, 233, 0.6);
-            }
-            .bp3-menu-item {
-              padding: 8px 12px;
-            }
-            .bp3-menu {
-              padding: 5px 0;
-            }
-            .bp3-menu-header {
-              padding: 8px 12px 8px 12px;
-            }
-            .bp3-overlay-backdrop {
-              background-color: rgba(0, 0, 0, 0.2);
-            }
-            /*
-            * react-circular-progressbar styles
-            * All of the styles in this file are configurable!
+        .CircularProgressbar {
+          /*
+            * This fixes an issue where the CircularProgressbar svg has
+          * 0 width inside a "display: flex" container, and thus not visible.
             */
-
-            .CircularProgressbar {
-              /*
-              * This fixes an issue where the CircularProgressbar svg has
-              * 0 width inside a "display: flex" container, and thus not visible.
-              */
-              width: 100%;
-              /*
-              * This fixes a centering issue with CircularProgressbarWithChildren:
-              * https://github.com/kevinsqi/react-circular-progressbar/issues/94
-              */
-              vertical-align: middle;
-            }
-
-            .CircularProgressbar .CircularProgressbar-path {
-              stroke: #3e98c7;
-              stroke-linecap: round;
-              transition: stroke-dashoffset 0.5s ease 0s;
-            }
-
-            .CircularProgressbar .CircularProgressbar-trail {
-              stroke: #d6d6d6;
-              /* Used when trail is not full diameter, i.e. when props.circleRatio is set */
-              stroke-linecap: round;
-            }
-
-            .CircularProgressbar .CircularProgressbar-text {
-              fill: #3e98c7;
-              font-size: 20px;
-              dominant-baseline: middle;
-              text-anchor: middle;
-            }
-
-            .CircularProgressbar .CircularProgressbar-background {
-              fill: #d6d6d6;
-            }
-
-            /*
-            * Sample background styles. Use these with e.g.:
-            *
-            *   <CircularProgressbar
-            *     className="CircularProgressbar-inverted"
-            *     background
-            *     percentage={50}
-            *   />
+          width: 100%;
+          /*
+            * This fixes a centering issue with CircularProgressbarWithChildren:
+            * https://github.com/kevinsqi/react-circular-progressbar/issues/94
             */
-            .CircularProgressbar.CircularProgressbar-inverted .CircularProgressbar-background {
-              fill: #3e98c7;
-            }
+          vertical-align: middle;
+        }
 
-            .CircularProgressbar.CircularProgressbar-inverted .CircularProgressbar-text {
-              fill: #fff;
-            }
+        .CircularProgressbar .CircularProgressbar-path {
+          stroke: #3e98c7;
+          stroke-linecap: round;
+          transition: stroke-dashoffset 0.5s ease 0s;
+        }
 
-            .CircularProgressbar.CircularProgressbar-inverted .CircularProgressbar-path {
-              stroke: #fff;
-            }
+        .CircularProgressbar .CircularProgressbar-trail {
+          stroke: #d6d6d6;
+          /* Used when trail is not full diameter, i.e. when props.circleRatio is set */
+            stroke-linecap: round;
+        }
 
-            .CircularProgressbar.CircularProgressbar-inverted .CircularProgressbar-trail {
-              stroke: transparent;
-            }
+        .CircularProgressbar .CircularProgressbar-text {
+          fill: #3e98c7;
+          font-size: 20px;
+          dominant-baseline: middle;
+          text-anchor: middle;
+        }
 
-            .bp3-transition-container {
-              z-index: 120;
-            }
-          `}</style>
-      </ThemeProvider>
+        .CircularProgressbar .CircularProgressbar-background {
+          fill: #d6d6d6;
+        }
+
+        /*
+          * Sample background styles. Use these with e.g.:
+          *
+          *   <CircularProgressbar
+        *     className="CircularProgressbar-inverted"
+        *     background
+        *     percentage={50}
+        *   />
+        */
+        .CircularProgressbar.CircularProgressbar-inverted .CircularProgressbar-background {
+          fill: #3e98c7;
+        }
+
+        .CircularProgressbar.CircularProgressbar-inverted .CircularProgressbar-text {
+          fill: #fff;
+        }
+
+        .CircularProgressbar.CircularProgressbar-inverted .CircularProgressbar-path {
+          stroke: #fff;
+        }
+
+        .CircularProgressbar.CircularProgressbar-inverted .CircularProgressbar-trail {
+          stroke: transparent;
+        }
+
+        .bp3-transition-container {
+          z-index: 120;
+        }
+        `}</style>
+    </ThemeProvider>
     )
   }
-}
+
+  export default MyApp
---
+++ utils/gtag.ts
@@ -0,0 +1,23 @@
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: string) => {
+  window.gtag('config', process.env.NEXT_PUBLIC_GA_TRACKING_ID, {
+    page_path: url,
+  })
+}
+
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+type EventProps = {
+	action: string
+	category: string
+	label: string	
+	value: number
+}
+export const event = ({ action, category, label, value }: EventProps) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+    debug_mode: process.env.NODE_ENV !== 'production'
+  })
+}
